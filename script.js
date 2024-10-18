d97b6f0eb3cdd54bbcdb31344a2ee49001077334
mohit <ms0135048@gmail.com>
2024-10-18 14:23:41 +0530

    implementing highscore functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let secretNumber=Math.trunc(Math.random()*20)+1;
 let score=20;
-let highScore=20;
+let highScore=0;
 
 
 document.querySelector('.check').addEventListener('click',()=>{
@@ -40,7 +40,11 @@ document.querySelector('.check').addEventListener('click',()=>{
     document.querySelector('.message').textContent = '🎉 Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    console.log(score);
+    //console.log(score);
+    if(score>highScore){
+        highScore=score;
+        document.querySelector(".highscore").textContent=highScore;
+    }
     
 }
 
@@ -53,8 +57,6 @@ document.querySelector('.again').addEventListener('click',()=>{
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('.score').textContent = score;
     document.querySelector('.guess').value='';
-  
-    
 
 });
 
